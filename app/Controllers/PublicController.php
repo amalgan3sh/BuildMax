36f92a150b48d628cc11ed28e00066b48715ed3d
Amal Ganesh <amal.ganesh@claysys.net>
2024-06-02 15:06:51 +0530

    initial commit

---
+++ app/Controllers/PublicController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class PublicController extends BaseController
+{
+    public function index(): string
+    {
+        // Load the header and the main view, combining them into a single output
+        $header = view('public/public_header');
+        $content = view('public/index');
+        $footer = view('public/public_footer');
+        
+        // Return the combined view content
+        return $header . $content.$footer;
+    }
+}
